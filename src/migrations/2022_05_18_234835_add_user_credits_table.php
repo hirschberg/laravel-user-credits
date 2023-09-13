cae43c3677a70fa2cc60762ed75c90946b888aad
Felix Hirschberg <felix@diescouts.de>
2023-09-13 14:44:53 +0200

    changed ID to biginteger

--- src/migrations/2022_05_18_234835_add_user_credits_table.php
+++ src/migrations/2022_05_18_234835_add_user_credits_table.php
@@ -17,7 +17,7 @@ class AddUserCreditsTable extends Migration
             $table->increments('id');
 
             //Foreign Key Referencing the id on the users table.
-            $table->integer('user_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->integer('amount')->default(0); //credit amount
